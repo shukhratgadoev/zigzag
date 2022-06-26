2f84961fa4c7670ec61a5efc80229359e30e1e2b
Shukhrat Gadoev <shukhratgadoev@77projects.com>
2022-06-26 18:56:38 +0500

    Initial commit

---
+++ zigzag.py
@@ -0,0 +1,22 @@
+from textwrap import indent
+import time, sys
+indent = 0
+indentIncreasing = True
+
+
+try:
+    while True:
+        print(''* indent, end='')
+        print('********')
+        time.sleep(0.1)
+        
+        if indentIncreasing:
+            indent = indent + 1
+            if indent == 20:
+                indentIncreasing = False
+        else:
+            indent = indent -1
+            if indent == 0:
+                indentIncreasing = True
+except KeyboardInterrupt:
+    sys.exit()
